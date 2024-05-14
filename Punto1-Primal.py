a53a8ed17d93ac97e8eca684627c99fc9301cd40
juanjomurillo12 <j.murilloa@uniandes.edu.co>
2024-05-14 00:33:21 -0500

    act

--- Punto1-Primal.py
+++ Punto1-Primal.py
@@ -8,26 +8,26 @@ import seaborn as sns
 # -----------------
 # Creación del objeto problema en PuLP
 # -----------------
-prob= plp.LpProblem("Dualidad",plp.LpMinimize)
+prob= plp.LpProblem("Dualidad",plp.LpMaximize)
 
 # -----------------------------
 # Variables de Decisión
 # -----------------------------
-w= {i:plp.LpVariable(f"w_{i}",lowBound=0,upBound=None,cat=plp.LpContinuous) for i in range(1,6)}
-r= {i:plp.LpVariable(f"r_{i}",lowBound=0,upBound=None,cat=plp.LpContinuous) for i in range(1,6)}
+x= {i:plp.LpVariable(f"x_{i}",lowBound=0,upBound=None,cat=plp.LpContinuous) for i in range(1,6)}
+s= {i:plp.LpVariable(f"s_{i}",lowBound=0,upBound=None,cat=plp.LpContinuous) for i in range(1,6)}
 # -----------------------------
 # Función Objetivo
 # -----------------------------
-prob += 105*w[1] + 765*w[2] + 220*w[3] - 120*w[4] + 225*w[5]
+prob += 17*x[1] + 30*x[2] - 4*x[3] + 5*x[4] - 10*x[5] + 0*s[1] + 0*s[2] + 0*s[3] + 0*s[4] + 0*s[5]
 
 # -----------------------------
 # Restricciones
 # -----------------------------
-prob += 7*w[1] + w[3] - w[4] - r[1] == 17
-prob += 2*w[2] + w[3] - r[2] == 30
-prob += 15*w[1] + 5*w[2] - 2*w[4] - r[3] == -4
-prob += 3*w[2] + 3*w[5] - r[4] == 5
-prob += -w[4] + w[5] - r[5] == -10
+prob += 7*x[1] + 15*x[3] + s[1] == 105
+prob += 2*x[2] + 5*x[3] + 3*x[4] + s[2] == 765
+prob += x[1] + x[2] + s[3] == 220
+prob += -x[1] - 2*x[3] - x[5] + s[4] == -120
+prob += 3*x[4] + x[5] + s[5] == 225
 
 # -----------------------------
 # Resolver el problema
@@ -40,8 +40,8 @@ prob.solve(plp.PULP_CBC_CMD(msg=0))
 print("Estado:", plp.LpStatus[prob.status])
 print("El valor de la F.O. es:", round(plp.value(prob.objective),2))
 for i in range(1,6):
-    print(f"w_{i} = {round(plp.value(w[i]),2)}")
-    print(f"r_{i} = {round(plp.value(r[i]),2)}")
+    print(f"x_{i} = {round(plp.value(x[i]),2)}")
+    print(f"s_{i} = {round(plp.value(s[i]),2)}")
 
 
 
